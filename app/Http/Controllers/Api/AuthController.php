4f525206c88e6c5fd867e2a2da9de6f02a7cca0a
Dev-Tams <tammyalemu@gmail.com>
2024-07-20 17:29:39 +0100

    Add method for sign in

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\StoreRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Transformers\UserTransformer;
 
 class AuthController extends Controller
 {
@@ -22,19 +24,60 @@ class AuthController extends Controller
      *  }
      **/
 
-    public function register(StoreRequest $request ){
+    public function register(StoreRequest $request, UserTransformer $transformer)
+    {
 
         $validatedData = $request->validated();
-      
-        $register = User::create([
-        'name' => $validatedData['name'],
-        'email' => $validatedData['email'],
-        'password' => $validatedData['password'],
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
         ]);
 
-        return response()
-        ->json($register, 201);
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        $responseData = $transformer->transform($user);
+
+        return response()->json($responseData, 201)->header(
+            'Authorization',
+            "Bearer $token",
+
+        )->header("Access-Control-Expose-Headers", "Authorization");
     }
 
-        
+
+     /**
+     * @ Logins a user
+     * 
+     * @transformer 200 App\Transformers\Api\UserTransformer
+     * @transformerModel App\Models\User resourceKey=user
+     * 
+     * @response 401 {
+     *    "errors": {
+     *      "key": ["Wrong input"]
+     *    }
+     *  }
+     **/
+
+    public function store(LoginRequest $request, UserTransformer $transformer)
+    {
+        //fetch user details,
+        $validatedData = $request->validated();
+
+
+        //verify details
+        if (!Auth::attempt($validatedData)) {
+            return response()->json(["error", "wrong credentials"], 401);
+        };
+
+        //login user
+       $user = Auth::user();
+
+       // $token = $user->accessToken('API Token')->plainTextToken;
+
+        $responseData = $transformer->transform($user);
+
+        return response()->json($responseData);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix("v1")->group(function () {
     Route::post("register", [AuthController::class, "register"]);
+    Route::post("user", [AuthController::class, "store"]);
 
     Route::middleware('auth:sanctum')->group(function (){
         //
